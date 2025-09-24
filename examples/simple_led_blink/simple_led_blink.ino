d83cd8064325893873f518fdb378114a5c88e960
MrDev-5000 <192122001+MrDev-5000@users.noreply.github.com>
2025-09-24 16:40:49 +0500

    Update simple_led_blink.ino

--- examples/simple_led_blink/simple_led_blink.ino
+++ examples/simple_led_blink/simple_led_blink.ino
@@ -1,11 +1,11 @@
 /**
- * @brief Examples sketch for blinking the built-in LED using toggleState() 
+ * @brief Example sketch for blinking the built-in LED using toggleState() 
  * @param duration in desired unit
  *
  * This example sketch demonstrates how to use the TimerKernel library to toggle the 
  * state of the built in led at regular time intervals.
  *
- * The duration can be specified in minutes, seconds, milliseconds, or microseconds.
+ * The duration can be specified in minutes, seconds, milliseconds or microseconds.
  * Uncomment the desired line in the loop() function to use a particular time unit.
  */
 
@@ -30,18 +30,13 @@ void loop() {
   * Only one of the following formats should be used at a time, Uncomment the desired line
   */ 
 
-  //digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInMins, MINUTE));     ///< Toggle state of led every 1/60 minute (1 second)
+  //digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInMins, MINUTE));     ///< Toggle built-in LED state every 1/60 minute (1 second)
 
-  ///or
+  //digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInSec, SECOND));     ///< Toggle built-in LED state every 1 second
 
-  //digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInSec, SECOND));     ///< Toggle state of led every 1 second
+  digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInMillisec));     ///<  Toggle built-in LED state every 1000 ms (1 second)
 
-  ///or
-
-  digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInMillisec));     ///<  Toggle state of led every 1000 millisecond (1 second)
-
-  ///or
-
-  //digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInMicrosec, MICROSECOND));     ///< Toggle state of led every 1000000 microsecond (1 second)
+  //digitalWrite(LED_BUILTIN, MyTimer.toggleState(durationInMicrosec, MICROSECOND));     ///< Toggle built-in LED state every 1,000,000 us (1 second)
 
 }
+
