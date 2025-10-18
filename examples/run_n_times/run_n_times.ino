628489e472f88828b9b457b61ed8deea87015e0a
MrDev-5000 <192122001+MrDev-5000@users.noreply.github.com>
2025-10-18 17:40:54 +0500

    Refactor run_n_times.ino to include runAmount constant
    Add: runAmount constant to specify number of times timer runs.
    fix: changed phrasing

--- examples/run_n_times/run_n_times.ino
+++ examples/run_n_times/run_n_times.ino
@@ -1,37 +1,36 @@
 /**
  * @file run_n_times.ino
- * @brief Example sketch for doing a task a specific number of times
- *
+ * @brief Example sketch for performing a task a specific number of times using TimerKernel
+ * 
+ * @details
  * This sketch shows:
  *  - Using hasExpired() to execute a task once after a set duration.
  *  - Using toggledState() to blink an LED infinitely at a given interval.
- *
  */
 
 #include <TimerKernel.h>
 
 TimerKernel MyTimer;
 
-// duration 1 second 
-const long durationInSec = 1;
+const unsigned long durationInSec = 1;     ///< time interval (1 second)
+const int runAmount = 1;          ///< Number of times the timer runs (1)
 
 void setup() {
   Serial.begin(9600);             ///< Start serial communication at 9600 baud
   pinMode(LED_BUILTIN, OUTPUT);   ///< Configure LED pin as output
-
 }
 
 void loop() {
   /**
-   * @brief Print "Hello world" once after 1 second
+   * @brief Print "Hello world" once after 1 second has passed.
    */
-  if (MyTimer.hasExpired(durationInSec, SECOND, 1)) {
+  if (MyTimer.hasExpired(durationInSec, SECOND, runAmount)) {
     Serial.println("Hello world");
   }
 
   /**
-   * @brief Blink the built-in LED infinitely at a 1-second interval
+   * @brief Blink the built-in LED infinitely (with 1 sec durations)
    */
   digitalWrite(LED_BUILTIN, MyTimer.toggledState(durationInSec, SECOND, TimerKernel::INFINITE));
-
 }
+
